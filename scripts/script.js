5fa81e63d8e8138e06e32ab223b811f99d5b8d31
SashoPetkov <petkovsasho@gmail.com>
2017-01-14 19:35:10 +0200

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -189,25 +189,30 @@
         var pauseInterval = false;
         
         var thisInterval = setInterval(changeSweetImg, 4000);
+        function rotates (){
+            // console.log(sweetRecipe[sweet_time-1].timeCook);    
+            sweet_rotate = sweetRecipe[sweet_image].img;
+            console.log(sweet_rotate);
+            changeImage = $('#section_3 .try').fadeOut('slow', function(){
+                $('#section_3 .try').css('background-image', 'url(' + sweet_rotate + ')');
+                $('#section_3 .time').text(sweetRecipe[sweet_time-1].timeCook);
+                $('#section_3 h2').text(sweetRecipe[sweet_time-1].title);
+                $('#section_3 p').text(sweetRecipe[sweet_time-1].description);
+            }).fadeIn('slow');                              // change main image
+            $('#sweetFullRec img').attr('src', sweet_rotate); // change image in recipe
+            productList.html(sweetRecipe[sweet_image].products); // change the list of products
+            howToDo.text(sweetRecipe[sweet_image].make_it); // change the made methood
+            sweet_image++;
+            sweet_time++;
+        }
         function changeSweetImg() {
             if (!pauseInterval) {                               // check is the interval is paused
                 if (sweet_image < sweetRecipe.length) {
-                    sweet_rotate = sweetRecipe[sweet_image].img;
-                    changeImage = $('#section_3 .try').fadeOut('slow', function(){
-                        $('#section_3 .try').css('background-image', 'url(' + sweet_rotate + ')');
-                        $('#section_3 .time').text(sweetRecipe[sweet_time - 1].timeCook);
-                        $('#section_3 h2').text(sweetRecipe[sweet_time - 1].title);
-                        $('#section_3 p').text(sweetRecipe[sweet_time - 1].description);
-                    }).fadeIn('slow');                              // change main image
-                    $('#sweetFullRec img').attr('src', sweet_rotate); // change image in recipe
-                    productList.html(sweetRecipe[sweet_image].products); // change the list of products
-                    howToDo.text(sweetRecipe[sweet_image].make_it); // change the made methood
-                    sweet_image++;
-                    sweet_time++;
-                    
+                    rotates();
                 } else { 
                     sweet_image = 0;
                     sweet_time = 0;
+                    rotates();
                 }
             }
 
